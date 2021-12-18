280f9e7f7dae1e6c463608a78aa0fb2b2b691a45
Szymon Datko <sdatko@redhat.com>
2021-12-18 17:33:20 +0100

    Improve testing of answers
    Now the script returns in color whether the output
    was correct or not and does not break checking
    after first failure. The proper exit status is set
    as well at the end of the script.

--- year-2020/test-answers.sh
+++ year-2020/test-answers.sh
@@ -48,6 +48,15 @@ EXPECTED_ANSWERS=(
 
 
 #
+# Cleanup function for the script, trap ensures to execute it
+#
+function cleanup() {
+    tput -T xterm sgr0
+}
+trap cleanup EXIT
+
+
+#
 # Have expected answers as command line arguments
 #
 set -- "${EXPECTED_ANSWERS[@]}"
@@ -56,6 +65,8 @@ set -- "${EXPECTED_ANSWERS[@]}"
 #
 # Test the solutions
 #
+error=0
+
 for DIRECTORY in "${WORK_DIR}/day-"*; do
     pushd "${DIRECTORY}" > /dev/null
 
@@ -63,19 +74,30 @@ for DIRECTORY in "${WORK_DIR}/day-"*; do
         EXPECTED_ANSWER=$1
         ACTUAL_ANSWER=$(python3 part-1.py)
         if [ "${ACTUAL_ANSWER}" != "${EXPECTED_ANSWER}" ]; then
-            exit 1
+            echo -ne "\033[31m"
+            error=1
+        else
+            echo -ne "\033[32m"
         fi
+        echo "---> ${DIRECTORY}/part-1.py"
         shift
     fi
 
     if [ -f 'part-2.py' ]; then
         EXPECTED_ANSWER=$1
         ACTUAL_ANSWER=$(python3 part-2.py)
+
         if [ "${ACTUAL_ANSWER}" != "${EXPECTED_ANSWER}" ]; then
-            exit 1
+            echo -ne "\033[31m"
+            error=1
+        else
+            echo -ne "\033[32m"
         fi
+        echo "---> ${DIRECTORY}/part-2.py"
         shift
     fi
 
     popd > /dev/null
 done
+
+exit "${error}"
--- year-2021/test-answers.sh
+++ year-2021/test-answers.sh
@@ -51,6 +51,15 @@ EXPECTED_ANSWERS=(
 
 
 #
+# Cleanup function for the script, trap ensures to execute it
+#
+function cleanup() {
+    tput -T xterm sgr0
+}
+trap cleanup EXIT
+
+
+#
 # Have expected answers as command line arguments
 #
 set -- "${EXPECTED_ANSWERS[@]}"
@@ -59,6 +68,8 @@ set -- "${EXPECTED_ANSWERS[@]}"
 #
 # Test the solutions
 #
+error=0
+
 for DIRECTORY in "${WORK_DIR}/day-"*; do
     pushd "${DIRECTORY}" > /dev/null
 
@@ -66,8 +77,12 @@ for DIRECTORY in "${WORK_DIR}/day-"*; do
         EXPECTED_ANSWER=$1
         ACTUAL_ANSWER=$(python3 part-1.py)
         if [ "${ACTUAL_ANSWER}" != "${EXPECTED_ANSWER}" ]; then
-            exit 1
+            echo -ne "\033[31m"
+            error=1
+        else
+            echo -ne "\033[32m"
         fi
+        echo "---> ${DIRECTORY}/part-1.py"
         shift
     fi
 
@@ -76,10 +91,16 @@ for DIRECTORY in "${WORK_DIR}/day-"*; do
         ACTUAL_ANSWER=$(python3 part-2.py)
 
         if [ "${ACTUAL_ANSWER}" != "${EXPECTED_ANSWER}" ]; then
-            exit 1
+            echo -ne "\033[31m"
+            error=1
+        else
+            echo -ne "\033[32m"
         fi
+        echo "---> ${DIRECTORY}/part-2.py"
         shift
     fi
 
     popd > /dev/null
 done
+
+exit "${error}"
