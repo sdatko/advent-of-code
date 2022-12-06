b58dd4b2f7325f2422e3e8762afc06559c53f1f2
Szymon Datko <szymon.datko@gmail.com>
2022-12-06 23:24:25 +0100

    Add template for solutions

---
+++ day-template/part-2.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+#
+# --- Day X: Title / Part Two ---
+#
+# .
+#
+#
+# --- Solution ---
+#
+# .
+#
+
+INPUT_FILE = 'input.txt'
+
+
+def main():
+    with open(INPUT_FILE, 'r') as file:
+        data = file.read().strip()
+
+    answer = len(data)
+
+    print(answer)
+
+
+if __name__ == '__main__':
+    main()
