ef3ff6296c5c97db116095e839058bb6dc271bf7
Szymon Datko <szymon.datko@gmail.com>
2023-12-19 16:11:00 +0100

    Update description for Day 18 / Year 2020

--- year-2020/day-18/part-2.py
+++ year-2020/day-18/part-2.py
@@ -1,13 +1,39 @@
 #!/usr/bin/env python3
 #
-# Task:
-# Now, addition and multiplication have different precedence levels, but
-# they're not the ones you're familiar with. Instead, addition is evaluated
-# before multiplication.
-# What do you get if you add up the results of evaluating the homework
-# problems using these new rules?
+# --- Day 18: Operation Order / Part Two ---
 #
-# Solution:
+# You manage to answer the child's questions and they finish part 1
+# of their homework, but get stuck when they reach the next section:
+# advanced math.
+#
+# Now, addition and multiplication have different precedence levels,
+# but they're not the ones you're familiar with. Instead, addition
+# is evaluated before multiplication.
+#
+# For example, the steps to evaluate the expression
+# 1 + 2 * 3 + 4 * 5 + 6 are now as follows:
+#
+# 1 + 2 * 3 + 4 * 5 + 6
+#   3   * 3 + 4 * 5 + 6
+#   3   *   7   * 5 + 6
+#   3   *   7   *  11
+#      21       *  11
+#          231
+#
+# Here are the other examples from above:
+#
+# – 1 + (2 * 3) + (4 * (5 + 6)) still becomes 51.
+# – 2 * 3 + (4 * 5) becomes 46.
+# – 5 + (8 * 3 + 9 + 3 * 4 * 3) becomes 1445.
+# – 5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4)) becomes 669060.
+# – ((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2 becomes 23340.
+#
+# What do you get if you add up the results of evaluating
+# the homework problems using these new rules?
+#
+#
+# --- Solution ---
+#
 # The only change is in how the calculation of formula without brackets works.
 # So we simply split the formula in places of multiplications, so we can first
 # perform additions on parts between multiplications signs, then we do multiply
