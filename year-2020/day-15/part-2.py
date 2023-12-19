aa0c0bab325336818f56d5ea8e03fda3a7ce0f02
Szymon Datko <szymon.datko@gmail.com>
2023-12-19 16:11:00 +0100

    Update description for Day 15 / Year 2020

--- year-2020/day-15/part-2.py
+++ year-2020/day-15/part-2.py
@@ -1,9 +1,23 @@
 #!/usr/bin/env python3
 #
-# Task:
+# --- Day 15: Rambunctious Recitation / Part Two ---
+#
+# Impressed, the Elves issue you a challenge: determine the 30000000th
+# number spoken. For example, given the same starting numbers as above:
+#
+# Given 0,3,6, the 30000000th number spoken is 175594.
+# Given 1,3,2, the 30000000th number spoken is 2578.
+# Given 2,1,3, the 30000000th number spoken is 3544142.
+# Given 1,2,3, the 30000000th number spoken is 261214.
+# Given 2,3,1, the 30000000th number spoken is 6895259.
+# Given 3,2,1, the 30000000th number spoken is 18.
+# Given 3,1,2, the 30000000th number spoken is 362.
+#
 # Given your starting numbers, what will be the 30000000th number spoken?
 #
-# Solution:
+#
+# --- Solution ---
+#
 # No changes, as the original algorithm proven to be effective enough.
 # I am not sure if anything can be done better here.
 #
