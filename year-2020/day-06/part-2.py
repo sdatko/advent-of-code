d4b59bcc906ec3c25a9f2f14e31720a4b4b0c841
Szymon Datko <szymon.datko@gmail.com>
2023-12-19 16:11:00 +0100

    Update description for Day 06 / Year 2020

--- year-2020/day-06/part-2.py
+++ year-2020/day-06/part-2.py
@@ -1,14 +1,51 @@
 #!/usr/bin/env python3
 #
-# Task:
-# The form asks a series of 26 yes-or-no questions marked a through z.
-# All you need to do is identify the questions for which anyone in group
-# answers "yes". Groups are separated by empty lines and one persons answers
-# are written in single line as questions marks that contained "yes".
+# --- Day 6: Custom Customs / Part Two ---
+#
+# As you finish the last group's customs declaration, you notice
+# that you misread one word in the instructions:
+#
+# You don't need to identify the questions to which anyone answered "yes";
+# you need to identify the questions to which everyone answered "yes"!
+#
+# Using the same example as above:
+#
+#   abc
+#
+#   a
+#   b
+#   c
+#
+#   ab
+#   ac
+#
+#   a
+#   a
+#   a
+#   a
+#
+#   b
+#
+# This list represents answers from five groups:
+#
+# – In the first group, everyone (all 1 person) answered "yes"
+#   to 3 questions: a, b, and c.
+# – In the second group, there is no question to which everyone
+#   answered "yes".
+# – In the third group, everyone answered yes to only 1 question, a.
+#   Since some people did not answer "yes" to b or c, they don't count.
+# – In the fourth group, everyone answered yes to only 1 question, a.
+# – In the fifth group, everyone (all 1 person) answered "yes"
+#   to 1 question, b.
+#
+# In this example, the sum of these counts is 3 + 0 + 1 + 1 + 1 = 6.
+#
 # For each group, count the number of questions to which
 # everyone answered "yes". What is the sum of those counts?
 #
-# Solution:
+#
+# --- Solution ---
+#
 # The only difference here is that we do intersection in groups to find
 # the same answers for everyone in group. Thank you Python :-)
 #
